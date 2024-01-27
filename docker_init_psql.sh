11c956ea8ec6377ae459ab28313e3bd8be309d1c
maaaaaaaaad <integral5137@gmail.com>
2024-01-28 01:54:34 +0900

    e

---
+++ docker_init_psql.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+echo "Enter database name:"
+read DB_NAME
+
+echo "Enter database user name:"
+read DB_USER
+
+echo "Enter database user password:"
+read DB_PASS
+
+INIT_SQL="/tmp/init.sql"
+DOT_ENV="./.env"
+
+if [ ! -f "$INIT_SQL" ]; then
+    echo "Creating init.sql..."
+    echo "CREATE DATABASE $DB_NAME;" >$INIT_SQL
+    echo "GRANT ALL PRIVILEGES ON DATABASE $DB_NAME TO $DB_USER;" >>$INIT_SQL
+else
+    echo "init.sql already exists."
+fi
+
+if [[ "$(docker images -q postgres 2>/dev/null)" == "" ]]; then
+    echo "Postgres image not found. Pulling from Docker Hub..."
+    docker pull postgres:14.10-alpine3.18
+else
+    echo "Postgres image found."
+fi
+
+docker run -d \
+    -e POSTGRES_USER=$DB_USER \
+    -e POSTGRES_PASSWORD=$DB_PASS \
+    -v $INIT_SQL:/docker-entrypoint-initdb.d/init.sql \
+    --name $DB_NAME \
+    postgres:14.10-alpine3.18
+
+echo "DATABASE_URL="postgresql://$DB_USER:$DB_PASS@localhost:5432/$DB_NAME"" >$DOT_ENV
